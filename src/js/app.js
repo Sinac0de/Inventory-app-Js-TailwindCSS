f109b8167c7ccdf139cd07cf4915159013321483
Sina Moradian <sinacsharp@gmail.com>
2022-08-04 08:38:48 +0430

    Create app.js

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+//target:
+//1.create category
+//2.create product based on selected category
+//3.edit product
+//4.remove product
+//5.save product
+// needed classes:
+// --> Storage class for handle application methods
+// --> ProductView class
+// --> CategoryView class
+// --> Main and App class
