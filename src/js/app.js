e4e67da3cae166a26192c2c5e3f64bfc6427a071
Sina Moradian <sinacsharp@gmail.com>
2022-08-06 14:20:22 +0430

    import CategoryView Class and methods

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,8 @@
-//target:
-//1.create category
-//2.create product based on selected category
-//3.edit product
-//4.remove product
-//5.save product
-// needed classes:
-// --> Storage class for handle application methods
-// --> ProductView class
-// --> CategoryView class
-// --> Main and App class
+import CategoryView from "./CategoryView.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    //set App => categories: ok
+    CategoryView.setApp();
+    //create categories options
+    CategoryView.createCategoriesList();
+});
